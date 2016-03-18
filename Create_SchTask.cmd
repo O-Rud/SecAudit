74555f0d55c9b19a877a45e3093238780e19f894
Oleksii.rud <tarnit@gmail.com>
2016-03-18 18:17:05 +0100

    Improved Scheduled task creation command

--- Create_SchTask.cmd
+++ Create_SchTask.cmd
@@ -1,1 +1,1 @@
-schtasks /create /S %1 /TN "Export EventLog to SQL" /RU "" /SC MINUTE /MO 15 /TR "powershell.exe -noprofile -NonInteractive -File \\HOME24.LAN\NETLOGON\SecAudit\Export-EventLog2SQLv3.ps1"
+schtasks /create /S %1 /TN "Export EventLog to SQL" /RU "" /SC MINUTE /MO 15 /TR "powershell.exe -noprofile -NonInteractive -ExecutionPolicy RemoteSigned -File \\HOME24.LAN\NETLOGON\SecAudit\Export-EventLog2SQLv3.ps1"
